3664d1ca77965aee4a55cf11e867cfbaad15ae84
OmarAZL <166664989+OmarAZL@users.noreply.github.com>
2026-06-18 20:01:09 -0400

    Add test endpoint to TestimonyController

--- Backend/src/main/java/naranja/custodia_360/controllers/TestimonyController.java
+++ Backend/src/main/java/naranja/custodia_360/controllers/TestimonyController.java
@@ -17,6 +17,11 @@ public class TestimonyController {
     private final TestimonyService testimonyService;
     private final AiService aiService;
 
+    @GetMapping()
+    public String testEndpoint() {
+        return "El Endpoint deberia funcionar";
+    }
+
     public TestimonyController(TestimonyService testimonyService, AiService aiService) {
 
         this.testimonyService = testimonyService;
